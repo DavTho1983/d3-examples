eec5767b65e4cd8f25a05a3e58758b41c763be9d
DavTho1983 <david.alex.thompson@gmail.com>
2020-10-20 21:43:42 +0100

    x axis almost done? Component symbols to follow

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import * as d3 from 'd3';
 import './App.css';
 
 function App() {
+  const [currentYear, setCurrentYear] = useState(2020);
+  const [maxYear, setMaxYear] = useState(30);
   const [data, setData] = useState(
     [
-      {x: 1.0, y1: 0.001, y2: 0.63},
-      {x: 2.0, y1: 0.003, y2: 0.84},
-      {x: 3.0, y1: 0.024, y2: 0.56},
-      {x: 4.0, y1: 0.054, y2: 0.22},
-      {x: 5.0, y1: 0.062, y2: 0.15},
-      {x: 6.0, y1: 0.100, y2: 0.08},
-      {x: 7.0, y1: 0.176, y2: 0.20},
-      {x: 8.0, y1: 0.198, y2: 0.71},
-      {x: 9.0, y1: 0.199, y2: 0.65},
+      {x: 2020, y1: 0.001, y2: 0.63},
+      {x: 2027, y1: 0.003, y2: 0.84},
+      {x: 2031, y1: 0.024, y2: 0.56},
+      {x: 2035, y1: 0.054, y2: 0.22},
+      {x: 2040, y1: 0.062, y2: 0.15},
+      {x: 2050, y1: 0.062, y2: 0.15}
     ]
   );
 
@@ -24,55 +23,37 @@ function App() {
     var pxY = svg.attr( "height" );
 
     var makeScale = function( accessor, range ) {
+      console.log("RANGE", accessor, range)
       return d3.scaleLinear()
         .domain( d3.extent( data, accessor ) )
-        .range( range ).nice()
+        .range( range )
+        .nice()
     }
 
-    var scX = makeScale( d => d["x"], [0, pxX] );
-    var scY1 = makeScale( d => d["y1"], [pxY, 0] );
-    var scY2 = makeScale( d => d["y2"], [pxY, 0] );
+    var scX = makeScale( d => d["x"], [0, pxX - 50]);
 
-    var drawData = function( g, accessor, curve ) {
-      g.selectAll( "circle" ).data(data).enter()
-        .append("circle")
-        .attr( "r", 5 )
-        .attr( "cx", d => scX(d["x"]) )
-        .attr( "cy", accessor );
 
-      var lnMkr = d3.line().curve( curve )
-        .x( d=>scX(d["x"]) ).y( accessor );
+    var g = d3.axisBottom( scX ).tickValues(
+      data.map(d => {
+        console.log("d x", d.x)
+        return d.x 
+      })
+    )
 
-      g.append( "path" ).attr( "fill", "none" )
-        .attr( "d", lnMkr( data ) );
-    }
-
-    var g1 = svg.append("g");
-    var g2 = svg.append("g");
-
-    drawData( g1, d => scY1(d["y1"]), d3.curveStep );
-    drawData( g2, d => scY2(d["y2"]), d3.curveNatural );
-
-    g1.selectAll( "circle" ).attr( "fill", "green" );
-    g1.selectAll( "path" ).attr( "stroke", "cyan" );
-
-    g2.selectAll( "circle" ).attr( "fill", "blue" );
-    g2.selectAll( "path" ).attr( "stroke", "red" );
-
-    var axMkr = d3.axisRight( scY1 );
-    axMkr( svg.append("g") );
-
-    axMkr = d3.axisLeft( scY2 );
 
     svg.append( "g" )
-    .attr("transform", "translate(" + pxX + ",0)" )
-    .call( axMkr );
+    .attr( "transform", "translate(" + 25 + "," + pxY/2 + ")")
+    .call( g );
 
-  svg.append( "g" ).call( d3.axisTop( scX ) )
-    .attr( "transform", "translate(0," + pxY + ")");
+    svg.selectAll(".domain").attr( "visibility", "hidden" );
+    svg.selectAll( ".tick" ).select("line").attr( "stroke", "red" ).attr( "stroke-width", 2)
 
+    // g.select( ".domain" ).attr( "visibility", "hidden" )
+    // g.selectAll( ".tick" ).select("line").attr( "stroke", "red" ).attr( "stroke-width", 2)
 
-  }
+    
+
+}
 
   useEffect(() => {
     if (data) {
@@ -83,8 +64,7 @@ function App() {
 
   return (
     <div className="App">
-      <svg id="demo1" width="600" height="300" style={{background: "lightgrey"}}/>
-
+      <svg id="demo1" width="1200" height="400" style={{background: "lightgrey"}}/>
     </div>
   );
 }
