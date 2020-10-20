4f6571704be5c6f76cddbf082fb979bccf3e7a94
DavTho1983 <david.alex.thompson@gmail.com>
2020-10-20 10:25:58 +0100

    example 3

--- src/App.js
+++ src/App.js
@@ -18,57 +18,65 @@ function App() {
   );
 
   const initialiseData = () => {
-    var pxX = 600, pxY = 300;
+    var svg = d3.select( "svg" );
 
-    var scX = d3.scaleLinear()
-      .domain( d3.extent(data, d => d["x"] ) )
-      .range( [0, pxX] );
+    var pxX = svg.attr( "width" );
+    var pxY = svg.attr( "height" );
 
-    var scY1 = d3.scaleLinear()
-      .domain(d3.extent(data, d => d["y1"] ) )
-      .range( [pxY, 0] );
+    var makeScale = function( accessor, range ) {
+      return d3.scaleLinear()
+        .domain( d3.extent( data, accessor ) )
+        .range( range ).nice()
+    }
 
-    var scY2 = d3.scaleLinear()
-      .domain(d3.extent(data, d => d["y2"] ) )
-      .range( [pxY, 0] );
+    var scX = makeScale( d => d["x"], [0, pxX] );
+    var scY1 = makeScale( d => d["y1"], [pxY, 0] );
+    var scY2 = makeScale( d => d["y2"], [pxY, 0] );
 
-    d3.select( "svg" )
-      .append( "g" ).attr( "id", "ds1")
-      .selectAll( "circle" )
-      .data(data).enter().append("circle")
-      .attr( "r", 5 ).attr( "fill", "green" )
-      .attr( "cx", d => scX(d["x"]) )
-      .attr( "cy", d => scY1(d["y1"]) );
+    var drawData = function( g, accessor, curve ) {
+      g.selectAll( "circle" ).data(data).enter()
+        .append("circle")
+        .attr( "r", 5 )
+        .attr( "cx", d => scX(d["x"]) )
+        .attr( "cy", accessor );
 
-    d3.select( "svg" )
-      .append( "g" ).attr( "id", "ds2")
-      .selectAll( "circle" )
-      .data(data).enter().append("circle")
-      .attr( "r", 5 ).attr( "fill", "blue" )
-      .attr( "cx", d => scX(d["x"]) )
-      .attr( "cy", d => scY2(d["y2"]) );
+      var lnMkr = d3.line().curve( curve )
+        .x( d=>scX(d["x"]) ).y( accessor );
 
-    var lineMaker = d3.line()
-      .x( d => scX( d["x"] ) )
-      .y( d => scY1( d["y1"] ) );
+      g.append( "path" ).attr( "fill", "none" )
+        .attr( "d", lnMkr( data ) );
+    }
 
-    d3.select( "#ds1" )
-      .append( "path" )
-      .attr( "fill", "none").attr( "stroke", "red" )
-      .attr( "d", lineMaker(data) );
+    var g1 = svg.append("g");
+    var g2 = svg.append("g");
 
-    lineMaker.y( d => scY2( d["y2"] ) );
+    drawData( g1, d => scY1(d["y1"]), d3.curveStep );
+    drawData( g2, d => scY2(d["y2"]), d3.curveNatural );
 
-    d3.select( "#ds2" )
-      .append( "path" )
-      .attr( "fill", "none").attr( "stroke", "cyan" )
-      .attr( "d", lineMaker(data) );
+    g1.selectAll( "circle" ).attr( "fill", "green" );
+    g1.selectAll( "path" ).attr( "stroke", "cyan" );
+
+    g2.selectAll( "circle" ).attr( "fill", "blue" );
+    g2.selectAll( "path" ).attr( "stroke", "red" );
+
+    var axMkr = d3.axisRight( scY1 );
+    axMkr( svg.append("g") );
+
+    axMkr = d3.axisLeft( scY2 );
+
+    svg.append( "g" )
+    .attr("transform", "translate(" + pxX + ",0)" )
+    .call( axMkr );
+
+  svg.append( "g" ).call( d3.axisTop( scX ) )
+    .attr( "transform", "translate(0," + pxY + ")");
+
 
   }
 
   useEffect(() => {
     if (data) {
-      initialiseData()
+      initialiseData();
     }
 
   }, [data])
