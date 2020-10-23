6a172f8d8d3a073223844b1191cb6b2ad32ba71a
DavTho1983 <david.alex.thompson@gmail.com>
2020-10-23 09:53:55 +0100

    x axis working

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import * as d3 from 'd3';
 import './App.css';
 
 function App() {
-  const [currentYear, setCurrentYear] = useState(2020);
-  const [maxYear, setMaxYear] = useState(30);
+  const [maxYear, setMaxYear] = useState();
   const [data, setData] = useState(
    [{
       x: 2020,
-      colour: "purple",
+      cx: 0,
+      colour: "#69306D",
       y1: 0,
       y2: 50,
       rad: 10,
-      amt: 300
+      amt: 0
     },
     {
-      x: 2029,
-      colour: "red",
+      x: 2031,
+      cx: 0,
+      colour: "#247BA0",
       y1: 0,
       y2: 50,
       rad: 10,
@@ -24,7 +25,8 @@ function App() {
     },
     {
       x: 2031,
-      colour: "yellow",
+      cx: 0,
+      colour: "#3F762C",
       y1: 0,
       y2: 50,
       rad: 10,
@@ -32,68 +34,98 @@ function App() {
     },
     {
       x: 2031,
-      colour: "green",
-      y1: 15,
-      y2: 50,
+      cx: 0,
+      colour: "#F25F5C",
+      y1: 0,
+      y2: 100,
       rad: 10,
       amt: 20
     },
     {
       x: 2031,
-      colour: "pink",
-      y1: 30,
-      y2: 50,
-      rad: 10,
-      amt: 1000
-    },
-    {
-      x: 2049,
-      colour: "blue",
+      cx: 0,
+      colour: "#0C3957",
       y1: 0,
-      y2: 50,
-      rad: 10,
-      amt: 200
-    },
-    {
-      x: 2050,
-      colour: "orange",
-      y1: 0,
-      y2: 50,
+      y2: 170,
       rad: 10,
       amt: 500
+    },
+    {
+      x: 2055,
+      cx: 0,
+      colour: "#BF802F",
+      y1: 0,
+      y2: 50,
+      rad: 10,
+      amt: 5000
     }
   ]
   );
 
   const initialiseData = () => {
+    for (let dotindex=0; dotindex<data.length; dotindex++) {
+      if (data[dotindex - 1]) {
+        if (data[dotindex - 1].x === data[dotindex].x) {
+          data[dotindex].y1 = data[dotindex -1].y1 -20
+        }
+      }
+    }
+
+    for (let circleCentreIndex=1; circleCentreIndex<data.length; circleCentreIndex++) {
+      data[circleCentreIndex].cx = data[circleCentreIndex].x
+      
+    }
+
+    const _data = data.reduce(
+        (r, v, _, __, k = v["x"]) => ((r[k] || (r[k] = [])).push(v), r),
+        []
+      )
+
+
     const svg = d3.select( "svg" );
     const pxX = svg.attr( "width" );
     const pxY = svg.attr( "height" );
+    let tickLabelOffset = 90;
+    
+    let minDotX = Math.min.apply(Math, data.map(function(o) { return o.y1; }))
+    if (minDotX < -20) {
+      tickLabelOffset += minDotX + 20;
+    }
 
-    const makeScale = ( accessor, range ) => {
+    const makeScale = ( arr, accessor, range ) => {
       console.log("RANGE", accessor, range)
       return d3.scaleLinear()
-        .domain( d3.extent( data, accessor ) )
+        .domain( d3.extent( arr, accessor ) )
         .range( range )
         .nice()
     }
 
-    const scX = makeScale( d => d["x"], [0, pxX - 200]);
+    const scX = makeScale( data, d => d.x, [0, pxX - 200]);
+
+    const thisYear = new Date().getFullYear()
     
-  
+    let tickTens = [];
+    for (let i=thisYear; i<maxYear; i++) {
+      if (i % 10 === 0) {
+        tickTens.push(i)
+      }
+    }
+
+    console.log("TICK TENS", tickTens)
+     
     const g = d3.axisBottom( scX ).tickValues(
-      data.map((d, index) => {
-        if (index > 0 && index !== data.length - 1) {
-          if (data[index].x !== data[index + 1].x && (data[index + 1].x && (!(data[index].x < data[index - 1].x - 1)) || !(data[index].x < data[index + 1].x - 1))) {
-            console.log("THIS D", data[index].x !== data[index - 1].x, d.x)
-            return ""
-          }
-        }
-        return d.x 
+      
+      tickTens.map((tickVal) => {
+        return tickVal
       })
     )
 
   const x = d3.scaleLinear()
+  .domain( d3.extent( data, d => d["x"] ) )
+  .range( [0, pxX - 200] )
+  .nice()
+
+  const x1 = d3.scaleLinear()
   .domain( d3.extent( data, d => d["x"] ) )
   .range( [0, pxX - 200] )
   .nice()
@@ -106,16 +138,20 @@ const y2 = d3.scaleLinear()
   .domain([0, 100])
   .range( [0, 100] );
 
+const y3 = d3.scaleLinear()
+  .domain([0, 100])
+  .range( [0, 50] );
+
 const rad = d3.scaleLinear()
   .domain(d3.extent(data, d => d.rad))
   .range([3, 10]);
 
-  const amt = d3.scaleLinear()
+const amt = d3.scaleLinear()
   .domain(d3.extent(data, d => d.amt))
-  .range([20, 100]);
+  .range([20, 50]);
 
     svg.append( "g" )
-    .attr( "transform", "translate(" + 50 + "," + pxY/2 + ")")
+    .attr( "transform", "translate(" + 50 + "," + (pxY - 200) + ")")
     .call( g )
     .selectAll(".tick text")
     .attr("fill", "#7A7A7A")
@@ -126,7 +162,7 @@ const rad = d3.scaleLinear()
       .append("g")
       .attr("class", "circles")
       .append("circle")
-      .attr( "transform", "translate(" + 100 + "," + 350 + ")")
+      .attr( "transform", "translate(" + 100 + "," + 650 + ")")
       .attr("fill", "white")
       .attr("stroke", d => d.colour)
       .attr("stroke-width", "2px")
@@ -134,27 +170,45 @@ const rad = d3.scaleLinear()
       .attr("cy", d => y1(d.y1))
       .attr("r", d => rad(d.rad));
 
+    let i = 0
+
     svg.selectAll("circle .circles")
       .data(data)
       .enter()
       .append("circle")
-      .attr( "transform", "translate(" + 100 + "," + 100 + ")")
-      .attr("fill", d => d.colour)
-      .attr("cx", d => x(d.x))
-      .attr("cy", d => y2(d.y2))
+      .attr( "transform", "translate(" + 100 + "," + 50 + ")")
+      .attr("fill", d => {
+        return d.colour}
+      )
+      .attr("cx", d => x1(d.x))
+      .attr("cy", (d, index) => {
+        console.log("HEY THERE")
+        let _y = y2(d.y2)
+        
+        if (_data[d.x].length > 1) {
+          i++
+          console.log("GROUP", _data[d.x][i -1].y2, i)
+          _y =  _data[d.x][i -1].y2
+        } else {
+          i = 0
+        }
+        return _y
+      })
       .attr("r", d => amt(d.amt));
 
     svg.selectAll(".domain")
       .attr("stroke", "#BDBDBD")
-      .attr("stroke-width", "4px")
-      .attr( "transform", "translate(" + 50 + "," + 120 + ")")
+      .attr("stroke-width", "2px")
+      .attr( "transform", "translate(" + 50 + "," + 150 + ")")
 
     svg.selectAll(".tick line")
-      .attr("visibility", "hidden")
+    .attr("stroke", "#BDBDBD")
+    .attr("stroke-width", "4px")
+    .attr( "transform", "translate(" + 50 + "," + 150 + ")")
       
     svg.selectAll( ".tick text")
       .attr("font-size", 20)
-      .attr( "transform", "translate(" + 0 + "," + 70 + ")")
+      .attr( "transform", "translate(" + 50 + "," + tickLabelOffset + ")")
       .attr("font-weight", "bold")
       .attr("dy", "0.5em")
       
@@ -164,14 +218,18 @@ const rad = d3.scaleLinear()
 
   useEffect(() => {
     if (data) {
+      setMaxYear(Math.max.apply(Math, data.map(function(o) { return o.x; })))
+    }
+
+    if (maxYear) {
       initialiseData();
     }
 
-  }, [data])
+  }, [data,maxYear])
 
   return (
     <div className="App">
-      <svg id="demo1" width="1200" height="400" style={{background: "white"}}/>
+      <svg id="demo1" width="1200" height="700" style={{background: "white"}}/>
     </div>
   );
 }
